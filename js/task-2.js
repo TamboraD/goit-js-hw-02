f648bd4e5bba568abeed42db200b511d2f812238
yuliia_mrachynska <mrachynska.y@gmail.com>
2024-11-09 15:35:11 +0300

    js

--- js/task-2.js
+++ js/task-2.js
@@ -5,6 +5,7 @@ return message
         return message.slice(0, maxLength) + " ..."
     }
 }
+
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
